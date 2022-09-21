306c12ee67258469095d8ea811e5fecd0e9ab029
kevinpoly659 <kevinpoly659@gmail.com>
2022-09-21 15:31:37 +0530

    Nav bar modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,9 @@ function sendemail(){
     message => alert(message)
   );
 }
+const navLinks = document.querySelectorAll(".nav-item")
+const menuToggle = document.getElementById("navbarNav")
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
