2d3b9c69f94958a018cbfb04d4bb4080117a6821
kevinpoly659 <kevinpoly659@gmail.com>
2022-09-29 01:25:13 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,9 @@ $("#submit-form").submit((e)=>{
         }
     })
 })
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
